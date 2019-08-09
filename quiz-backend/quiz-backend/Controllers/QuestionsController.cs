a26669e5398903bb38ef8eb23498495e3b725a46
KonstantinDinev <konstDinev@gmail.com>
2019-08-09 15:08:33 +0300

    initial project

---
+++ quiz-backend/quiz-backend/Controllers/QuestionsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using quiz_backend.Models;
+
+namespace quiz_backend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Questions")]
+    public class QuestionsController : ControllerBase
+    {
+        readonly QuizContext _context;
+
+        public QuestionsController(QuizContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpPost]
+        public Question Post([FromBody]Question question)
+        {
+            _context.Questions.Add(question);
+            _context.SaveChanges();
+
+            return new Question { text = "Thanks" };
+        }
+
+        // GET api/questions
+        [HttpGet]
+        public ActionResult<IEnumerable<Question>> Get()
+        {
+            return new Question[] {
+                new Models.Question() { text = "first question" },
+                new Models.Question() { text = "2nd question" }
+            };
+        }
+    }
+}
---
+++ quiz-backend/quiz-backend/QuizContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace quiz_backend
+{
+    public class QuizContext : DbContext
+    {
+        public DbSet<Models.Question> Questions { get; set; }
+
+        public QuizContext(DbContextOptions<QuizContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
