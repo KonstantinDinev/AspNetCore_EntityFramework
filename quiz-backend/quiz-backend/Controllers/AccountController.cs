4d5b12747eeb290b1fb1f7ba59901e27f1cbaa1b
KonstantinDinev <konstDinev@gmail.com>
2019-08-14 14:49:09 +0100

    Setup Identity Framework; Register a new User, return a token

---
+++ quiz-backend/quiz-backend/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace quiz_backend.Controllers
+{
+    public class Credentials
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        readonly UserManager<IdentityUser> _userManager;
+        readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this._userManager = userManager;
+            this._signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] Credentials credentials)
+        {
+            var user = new IdentityUser { UserName = credentials.Email, Email = credentials.Email };
+
+            var result = await this._userManager.CreateAsync(user, credentials.Password);
+
+            if (!result.Succeeded)
+                return BadRequest(result.Errors);
+
+            await this._signInManager.SignInAsync(user, isPersistent: false);
+
+            var jwt = new JwtSecurityToken();
+
+            return Ok(new JwtSecurityTokenHandler().WriteToken(jwt));
+        }
+    }
+}
