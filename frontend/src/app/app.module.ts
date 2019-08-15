0b694183995285ccb3309fbf2774a31f00daff74
KonstantinDinev <konstDinev@gmail.com>
2019-08-15 11:45:03 +0100

    Add HttpInterceptor in between every request adding an Authorization header with Bearer token for each request

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { 
   MatButtonModule,
@@ -24,6 +24,7 @@ import { QuizComponent } from './quiz.component';
 import { QuizzesComponent } from './quizzes.component';
 import { RegisterComponent } from './register.component';
 import { AuthService } from './auth.service';
+import { AuthInterceptor } from './auth.interceptor';
 
 const routes = [
   { path: '', component: HomeComponent },
@@ -61,7 +62,11 @@ const routes = [
     MatCheckboxModule,
     MatToolbarModule
   ],
-  providers: [ApiService, AuthService],
+  providers: [ApiService, AuthService, {
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/src/app/auth.service.ts
+++ frontend/src/app/auth.service.ts
@@ -10,7 +10,6 @@ export class AuthService {
     register(credentials) {
         this.http.post<any>(`http://localhost:60763/api/account/`, credentials)
             .subscribe(res => {
-                console.log(res);
                 localStorage.setItem('token', res);
                 console.log(localStorage.getItem('token'));
             });
